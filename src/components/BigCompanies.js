ae79a4fc9ba6d3246ac2436cba9ccf170c793e9e
xxsitiafrida <sitiafrida1234@gmail.com>
2023-07-10 16:52:06 +0700

    image companies

--- src/components/BigCompanies.js
+++ src/components/BigCompanies.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
+import Surveyor from "../assets/image/Image30.png";
+import Krakatau from "../assets/image/krakatau EXT 1.png";
 
-export default function BigCompanies (){
+export default function BigCompanies() {
     return (
-        <div className='bg bg-color-primary' style={{
-            backgroundColor: '#559FF8',
-            width: '100%', 
-            height: '200px'}}
-            >ncbnskxxzmcsdcjsdncjndnscjknsjdc
-            </div>
-    )
-}
+        <div className='bg bg-color-primary' style={{ backgroundColor: '#559FF8', width: '100%', height: '200px' }}>
+        <div className="container mx-auto flex justify-items-center items-center">
+          <img src={Surveyor} alt="Surveyor Indonesia" />
+          <img src={Krakatau} alt="Krakatau" />
+        </div>
+      </div>
+      
+      
+    );
+    }
